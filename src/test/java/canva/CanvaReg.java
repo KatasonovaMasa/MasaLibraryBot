ea35f4da5ebc9deef3e19a581c1f7616c6d0083c
KatasonovaMasa <katasonova_masa@mail.ru>
2020-08-06 22:13:06 +0300

    Add true selectors

--- src/test/java/canva/CanvaReg.java
+++ src/test/java/canva/CanvaReg.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,21 +17,32 @@ public class CanvaReg {
     public void canvaReg(){
         System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver1");
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().setSize(new Dimension(1000,1000));
-        driver.get("https://www.canva.com/");
-        WebElement button = driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/button"));
+        driver.manage().window().setSize(new Dimension(1200,1200));
+        driver.get("https://www.canva.com");
+        WebElement button = driver.findElement(By.xpath("//html/body/div[3]/div/main/div[6]/div/section/div/div/div/div/div/div/button"));
         button.click();
+
+        //убеждаемся, что браузер открылся в течении 10 сек
         WebDriverWait  wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/div[2]/form/button")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("form")));
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/div[2]/form/div[1]/input")).sendKeys("Maru");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html/body/div[3]/div/main/div[6]/div/section/div")));
+
+        driver.findElement(By.xpath("//*[@name='name']")).sendKeys("Maru");
+
+        //добавляем метод рандомного добавления числа, для уникальности email
         Random random = new Random();
         int n = random.nextInt(100) +1;
-        String email = "katasonomasa" + n + "@gmail.com";
+        String email = "katasonova_masa" + n + "@gmail.com";
         System.out.println(email);
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/div[2]/form/div[2]/div/div/div/input")).sendKeys(email);
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/div[2]/form/div[3]/input")).sendKeys("zaqplM123!@");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[6]/div/section/div/div/div/div/div/div/div[2]/form/button/span")).click();
-        driver.quit();
+
+        //ищем элементы формы и заполняем данные
+        driver.findElement(By.xpath("//*[@name='email']")).sendKeys(email);
+        driver.findElement(By.cssSelector("input[type=\"password\"]")).sendKeys("zaqplM123!@");
+        driver.findElement(By.xpath("//span[text()='Начните работу! Это бесплатно.']")).click();
+
+        //проверяем проверку путем появления элемента после регистрации
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html/body/div[2]/div/div/div[3]/div/div/div/div/div/div")));
+
+        //закрывем браузер
+         driver.quit();
     }
 }
