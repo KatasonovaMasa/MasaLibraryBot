7b51c509b90ea6febd29b6e73da8b53128facd61
Katasonova Masa <katasonova_masa@mail.ru>
2022-10-04 17:05:14 +0300

    bot

---
+++ MasaLibraryBot/MasaLibraryBot/src/main/java/com/example/MasaLibraryBot/MasaLibraryBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.MasaLibraryBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasaLibraryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasaLibraryBotApplication.class, args);
+	}
+
+}
---
+++ MasaLibraryBot/MasaLibraryBot/src/test/java/com/example/MasaLibraryBot/MasaLibraryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MasaLibraryBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasaLibraryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
